2a4b7859a2f717b406cb3e3af7eb1ebc9872c267
luizfelipew <luiz.f.campos@partner.bmw.com.br>
2023-12-13 15:50:38 -0300

    testando código formatado

--- index.mjs
+++ index.mjs
@@ -1,19 +1,18 @@
 import { log } from './log.mjs';
 import { S3 } from '@aws-sdk/client-s3';
 
-
 const s3Client = new S3({ region: 'us-east-1' });
 
 export const handler = async(event) => {
-    const record = event.record[0];
+    const record = event.Records[0];
     const Bucket = record.s3.bucket.name;
     const Key = record.s3.object.key;
 
     const getObjectResult = await s3Client.getObject({
         Bucket,
-        Key
+        Key,
     });
-
+    
     const mega_byte = 1024 * 1024;
 
     if (getObjectResult.ContentLength > 1 * mega_byte) {
@@ -22,6 +21,7 @@ export const handler = async(event) => {
         return 'Objeto muito grande';
     }
 
-    log('Objeto de tamanho ok');
-    return 'Objeto de tamanho ok';
+    log('Objeto de tamanho OK');
+    
+    return 'Objeto de tamanho OK';
 };
